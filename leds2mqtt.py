18eded050e16434e412fdbd419f1585c4e1c0c44
Matthias <mame@plattform32.de>
2019-01-27 16:10:20 +0100

    Add Gamma Correction

--- leds2mqtt.py
+++ leds2mqtt.py
@@ -1,4 +1,5 @@
 import time
+import colorsys
 import paho.mqtt.client as mqtt
 
 import RPi.GPIO as GPIO
@@ -12,7 +13,26 @@ PIXEL_COUNT = 64
 SPI_PORT   = 0
 SPI_DEVICE = 0
 pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
-ledson = 42 # debouncing HASS repeated ON commands
+ledson = True # debouncing HASS repeated ON commands
+
+# https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+# Avoid to bright colors
+GAMMA_LUT = [    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255]
 
 
 def on_connect(client, userdata, flags, rc):
@@ -42,6 +62,8 @@ def on_message(client, userdata, msg):
         elif 'OFF':
             print("LEDS off")
             leds_off()
+    if msg.topic == 'ledstripe/brightness/set' :
+        print("LEDs brightness:" + msg.payload.decode())
     if msg.topic == 'ledstripe/rgb/set' :
         print("LEDs RGB:" + msg.payload.decode())
         rgb = msg.payload.decode().split(',')
@@ -57,12 +79,24 @@ def leds_off():
 def leds_on():
     global ledson
     ledson = True
-    pixels.set_pixels(Adafruit_WS2801.RGB_to_color(0,0,64))
+    pixels.set_pixels(Adafruit_WS2801.RGB_to_color(0,0,10))
     pixels.show()
 
 def leds_rgb(r,g,b):
+    r = GAMMA_LUT[r]
+    g = GAMMA_LUT[g]
+    b = GAMMA_LUT[b]
     pixels.set_pixels(Adafruit_WS2801.RGB_to_color(r,g,b))
     pixels.show()
+    
+
+"""
+def leds_brightness(perc):
+    for i in range(PIXEL_COUNT):
+        c = pixels.get_pixel_rgb(i)
+        hsv =  colorsys.rgb_to_hsv(c)
+        c = 
+"""
 
 def blink_color(pixels, blink_times=5, wait=0.5, color=(255,0,0)):
     for i in range(blink_times):
